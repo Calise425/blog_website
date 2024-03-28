833a05463399a9e17d80902c4ccdafd3092c19f2
Calise425 <caitlin.norris1998@gmail.com>
2024-03-27 21:24:37 -0600

    Added additional fake blog posts on npoint, home page now renders those blogs and the links to each blog will bring up their own page. TODO fix contact page functionality

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ import requests
 
 app = Flask(__name__)
 
-all_posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+all_posts = requests.get("https://api.npoint.io/7f0c3bb1cde9939e81db").json()
 
 
 @app.route('/')
 def home():
     return render_template("index_bs.html", posts=all_posts)
+
+
+@app.route('/about')
+def about_page():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact_page():
+    return render_template("contact.html")
 
 
 @app.route('/blog/<blog_id>')
@@ -17,7 +27,7 @@ def get_blog(blog_id):
     for post in all_posts:
         if post["id"] == int(blog_id):
             selected_post = post
-    return render_template("post.html", post=selected_post)
+    return render_template("post_bs.html", post=selected_post)
 
 
 if __name__ == "__main__":
